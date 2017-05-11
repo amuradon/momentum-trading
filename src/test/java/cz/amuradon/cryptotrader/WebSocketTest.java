30ea8a84faa23f6e4ded1c73408c7398ab770265
Martin Augusta <martin.augusta@gmail.com>
2017-05-11 17:40:14 +0200

    Jawampa redirected to Camel using Camel-RX

--- src/test/java/cz/amuradon/cryptotrader/WebSocketTest.java
+++ src/test/java/cz/amuradon/cryptotrader/WebSocketTest.java
@@ -2,9 +2,14 @@ package cz.amuradon.cryptotrader;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.rx.ReactiveCamel;
 import org.junit.Test;
 
 import rx.functions.Action1;
+import ws.wamp.jawampa.ApplicationError;
 import ws.wamp.jawampa.WampClient;
 import ws.wamp.jawampa.WampClientBuilder;
 import ws.wamp.jawampa.connection.IWampConnectorProvider;
@@ -14,27 +19,48 @@ public class WebSocketTest {
 
 	@Test
 	public void test() throws Exception {
-		WampClient client;
-		WampClientBuilder builder = new WampClientBuilder();
-		IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
-		builder.withConnectorProvider(connectorProvider).withUri("wss://api.poloniex.com").withRealm("realm1")
-				.withInfiniteReconnects().withReconnectInterval(5, TimeUnit.SECONDS);
-		client = builder.build();
+		final WampClient client = createWampClient();
+		final ReactiveCamel rxCamel = createRxCamel();
 
 		client.statusChanged().subscribe(new Action1<WampClient.State>() {
 
 			@Override
 			public void call(WampClient.State t1) {
 				if (t1 instanceof WampClient.ConnectedState) {
-					// subscription =
-					client.makeSubscription("ticker").subscribe((s) -> {
-						System.out.println(s.arguments());
-					});
+					rxCamel.sendTo(client.makeSubscription("ticker"), "direct://websocket");
 				}
 			}
 		});
 		client.open();
 
 		Thread.sleep(10000);
+	}
+
+	private WampClient createWampClient() throws ApplicationError, Exception {
+		final WampClientBuilder builder = new WampClientBuilder();
+		final IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
+		builder.withConnectorProvider(connectorProvider)
+				.withUri("wss://api.poloniex.com")
+				.withRealm("realm1")
+				.withInfiniteReconnects()
+				.withReconnectInterval(5, TimeUnit.SECONDS);
+		final WampClient client = builder.build();
+		return client;
+	}
+	
+	private ReactiveCamel createRxCamel() throws Exception {
+		final CamelContext camel = new DefaultCamelContext();
+		camel.addRoutes(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				// The body type is PubSubData
+				from("direct://websocket")
+						.log("${body.arguments}");
+			}
+		});
+		camel.start();
+		
+		return new ReactiveCamel(camel);
 	}
 }

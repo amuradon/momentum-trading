0e57c72965c037c8120e13ca8e9275ab2fc50210
Martin Augusta <martin.augusta@gmail.com>
2017-05-17 17:00:16 +0200

    #3 Serialization

--- src/main/java/cz/amuradon/cryptotrader/MyMain.java
+++ src/main/java/cz/amuradon/cryptotrader/MyMain.java
@@ -1,15 +1,35 @@
 package cz.amuradon.cryptotrader;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.main.Main;
+import org.apache.camel.main.MainListenerSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyMain {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyMain.class);
+	
 	private static Main main;
 
 	public static void main(String[] args) throws Exception {
 		main = new Main();
-		main.addRouteBuilder(new WebSocketRouter());
+		final WebSocketRouter wsBuilder = new WebSocketRouter();
+		main.addRouteBuilder(wsBuilder);
 		main.addRouteBuilder(new RestRouter());
+		
+		main.addMainListener(new MainListenerSupport() {
+			
+			@Override
+			public void configure(CamelContext context) {
+				try {
+					wsBuilder.runWampClient(context);
+				} catch (Exception e) {
+					LOGGER.error("Oh, shit.", e);
+				}
+			}
+		});
+		
 		main.run();
 	}
 }
--- src/main/java/cz/amuradon/cryptotrader/WebSocketRouter.java
+++ src/main/java/cz/amuradon/cryptotrader/WebSocketRouter.java
@@ -2,11 +2,16 @@ package cz.amuradon.cryptotrader;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.rx.ReactiveCamel;
 
 import rx.functions.Action1;
 import ws.wamp.jawampa.ApplicationError;
+import ws.wamp.jawampa.PubSubData;
 import ws.wamp.jawampa.WampClient;
 import ws.wamp.jawampa.WampClientBuilder;
 import ws.wamp.jawampa.connection.IWampConnectorProvider;
@@ -15,16 +20,24 @@ import ws.wamp.jawampa.transport.netty.NettyWampClientConnectorProvider;
 
 public class WebSocketRouter extends RouteBuilder {
 
+	public static final String DIRECT_WEBSOCKET = "direct://websocket";
+
 	@Override
 	public void configure() throws Exception {
 		// The body type is PubSubData
-		from("direct://websocket")
-				.log("${body.arguments}");
-		
-		runWampClient();
+		from(DIRECT_WEBSOCKET)
+				.transform(new Expression() {
+					
+					@Override
+					public <T> T evaluate(Exchange exchange, Class<T> type) {
+						return exchange.getContext().getTypeConverter().convertTo(type,
+								new Tick(exchange.getIn().getBody(PubSubData.class).arguments()));
+					}
+				})
+				.log("${body}");
 	}
 
-	private void runWampClient() throws ApplicationError, Exception {
+	public void runWampClient(CamelContext camelContext) throws ApplicationError, Exception {
 		final WampClientBuilder builder = new WampClientBuilder();
 		final IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
 		builder.withConnectorProvider(connectorProvider)
@@ -34,14 +47,14 @@ public class WebSocketRouter extends RouteBuilder {
 				.withReconnectInterval(5, TimeUnit.SECONDS);
 		final WampClient client = builder.build();
 		
-		final ReactiveCamel rxCamel = new ReactiveCamel(getContext());
+		final ReactiveCamel rxCamel = new ReactiveCamel(camelContext);
 
 		client.statusChanged().subscribe(new Action1<WampClient.State>() {
 
 			@Override
 			public void call(WampClient.State t1) {
 				if (t1 instanceof WampClient.ConnectedState) {
-					rxCamel.sendTo(client.makeSubscription("ticker"), "direct://websocket");
+					rxCamel.sendTo(client.makeSubscription("ticker"), DIRECT_WEBSOCKET);
 				}
 			}
 		});

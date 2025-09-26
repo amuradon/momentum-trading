232ad8ac0cd7901cefb1f3e9084e84c70b5b33fa
Martin Augusta <martin.augusta@gmail.com@>
2025-09-26 13:33:24 +0200

    T #2 some fixes

--- src/main/java/cz/amuradon/tralon/motrade/Scanner.java
+++ src/main/java/cz/amuradon/tralon/motrade/Scanner.java
@@ -2,12 +2,10 @@ package cz.amuradon.tralon.motrade;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -17,41 +15,45 @@ import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class ScheduledJobs {
+public class Scanner {
 	
 	private final MexcClient mexcClient;
 	
 	private final Map<String, LinkedList<SymbolValues>> movingValues;
 
-	public ScheduledJobs(@RestClient final MexcClient mexcClient) {
+	public Scanner(@RestClient final MexcClient mexcClient) {
 		this.mexcClient = mexcClient;
 		this.movingValues = new HashMap<>();
 	}
 	
+	// TODO 
 	@Startup
 	void init() {
 		Ticker[] tickers = mexcClient.ticker();
 		for (Ticker ticker : tickers) {
-			// Filtering out non-USDT quote pairs
-			if (ticker.symbol().endsWith("USDT")) {
-				LinkedList<SymbolValues> values = new LinkedList<>();
-				for (int i = 0; i < 15; i++) {
-					values.add(new SymbolValues(ticker.lastPrice(), ticker.quoteVolume()));
-				}
+			if (filter(ticker)) {
+				LinkedList<SymbolValues> values = initiateValues(ticker);
 				movingValues.put(ticker.symbol(), values);
 			}
 		}
 	}
+
+	private LinkedList<SymbolValues> initiateValues(Ticker ticker) {
+		LinkedList<SymbolValues> values = new LinkedList<>();
+		for (int i = 0; i < 15; i++) {
+			values.add(new SymbolValues(ticker.lastPrice(), ticker.quoteVolume()));
+		}
+		return values;
+	}
 	
-	@Scheduled(cron = "0 * * * * ?", delay = 1)
+	@Scheduled(cron = "0 * * * * ?", delay = 40, delayUnit = TimeUnit.SECONDS)
 	void mexcTicker() {
 		Ticker[] tickers = mexcClient.ticker();
 		for (Ticker ticker : tickers) {
-			// Filtering out non-USDT quote pairs
-			if (ticker.symbol().endsWith("USDT")) {
-				LinkedList<SymbolValues> values = movingValues.get(ticker.symbol());
+			if (filter(ticker)) {
+				LinkedList<SymbolValues> values = movingValues.computeIfAbsent(ticker.symbol(), k -> initiateValues(ticker));
 				SymbolValues m15 = values.getFirst();
-				SymbolValues m5 = values.get(9);  // XXX Tohle muze byt zbytecne drahe u LinkedList
+				SymbolValues m5 = values.get(9);
 				SymbolValues m1 = values.getLast();
 				
 				// Spocitat % rozdilu pro cenu a volume
@@ -63,8 +65,8 @@ public class ScheduledJobs {
 				BigDecimal m15PriceChange = getPercentage(lastPrice, m15.lastPrice());
 				if (aboveThreshold(m1PriceChange) || aboveThreshold(m5PriceChange) || aboveThreshold(m15PriceChange)) {
 					BigDecimal m1VolumeChange = getPercentage(quoteVolume, m1.quoteVolume());
-					BigDecimal m5VolumeChange = getPercentage(quoteVolume, m1.quoteVolume());
-					BigDecimal m15VolumeChange = getPercentage(quoteVolume, m1.quoteVolume());
+					BigDecimal m5VolumeChange = getPercentage(quoteVolume, m5.quoteVolume());
+					BigDecimal m15VolumeChange = getPercentage(quoteVolume, m15.quoteVolume());
 					Log.infof("Symbol %s, 1p: %s, 5p: %s, 15p: %s, 1v: %s, 5v: %s, 15v: %s", ticker.symbol(),
 							m1PriceChange, m5PriceChange, m15PriceChange, m1VolumeChange, m5VolumeChange,
 							m15VolumeChange);
@@ -75,7 +77,6 @@ public class ScheduledJobs {
 		}
 	}
 
-	// FIXME evidentne muze nejaky BigDecimal byt 0 -> deleni nuloul
 	private BigDecimal getPercentage(BigDecimal current, BigDecimal previous) {
 		return current.subtract(previous).divide(safeZero(previous), 4, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100));
 	}
@@ -85,6 +86,10 @@ public class ScheduledJobs {
 	}
 	
 	private boolean aboveThreshold(BigDecimal value) {
-		return value.compareTo(BigDecimal.valueOf(5)) > 1;
+		return value.compareTo(BigDecimal.valueOf(5)) == 1;
+	}
+	
+	private boolean filter(Ticker ticker) {
+		return ticker.symbol().endsWith("USDT") && ticker.quoteVolume().compareTo(BigDecimal.valueOf(50000)) == 1;
 	}
 }
